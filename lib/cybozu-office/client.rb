756f9552698c2e34159006cb3fb92bea78c773f8
fukata <tatsuya.fukata@gmail.com>
2016-02-10 16:05:14 +0700

    fix add url suffix

--- lib/cybozu-office/client.rb
+++ lib/cybozu-office/client.rb
@@ -6,7 +6,8 @@ module CybozuOffice
 
     def initialize(option={})
       @option = option
-      @option[:url] ||= ENV['CYBOZU_OFFICE_URL'] + "?page=PApi"
+      @option[:url] ||= ENV['CYBOZU_OFFICE_URL']
+      @option[:url] += "?page=PApi" if @option[:url]
       @option[:username] ||= ENV['CYBOZU_OFFICE_USERNAME']
       @option[:password] ||= ENV['CYBOZU_OFFICE_PASSWORD']
       puts @option if @option[:debug]
