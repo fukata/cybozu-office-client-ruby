e46040d405f0753502e6dfa830dbab7340ab3eed
fukata <tatsuya.fukata@gmail.com>
2018-01-16 08:15:06 +0900

    encode xml

--- lib/cybozu-office/api/message.rb
+++ lib/cybozu-office/api/message.rb
@@ -14,12 +14,12 @@ module CybozuOffice
         file_xml = opt[:files].each_with_index.map do |file,i|
           %Q{<file id="attached#{i}"><content>#{Base64.encode64(File.binread(file))}</content></file>}
         end.join("\n") if opt[:files]
-       
+
         content_xml = %Q{<content xmlns="http://schemas.cybozu.co.jp/message/2008"}
-        content_xml << %Q{ body="#{opt[:body]}"}
-        content_xml << %Q{ html_body="#{opt[:html_body]}"} if opt.key?(:html_body)
+        content_xml << %Q{ body="#{opt[:body].to_s.encode(xml: :text)}"}
+        content_xml << %Q{ html_body="#{opt[:html_body].to_s.encode(xml: :text)}"} if opt.key?(:html_body)
         content_xml << %Q{>}
-        content_xml << file_content_xml.to_s 
+        content_xml << file_content_xml.to_s
         content_xml << %Q{</content>}
 
         params = <<-EOS
